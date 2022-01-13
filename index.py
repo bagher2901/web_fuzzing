1558d0fc267a0764c9fca9c06da8e724b7c4842e
bagher2901 <bagher.m1377@gmail.com>
2022-01-13 22:00:41 +0330

    add Beautiful Soup

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print("                 hi         ")
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
+link_address = input('enter your website address       >>   ')
+link_address = "https://www."+ link_address
+print(link_address)
+req = Request(link_address)
+html_page = urlopen(req)
+
+soup = BeautifulSoup(html_page, "html5lib")
+
+links = []
+for link in soup.findAll('a'):
+    links.append(link.get('href'))
+
+print(links)
